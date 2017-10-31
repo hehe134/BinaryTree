4b0e23faaa39b6d5c453176105aafbf7a4b6b837
zhongqi <624623296@qq.com>
2017-11-01 00:58:03 +0300

    BinaryTree

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -1,11 +1,8 @@
 
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class BinaryTree {
-    public TreeNode root = null;
-    private List<TreeNode> nodeList = new ArrayList<TreeNode>();
+    TreeNode root = null;
+//    private List<TreeNode> nodeList = new ArrayList<TreeNode>();
 
     private class TreeNode {
         int key;
@@ -13,24 +10,24 @@ public class BinaryTree {
         TreeNode rightChild;
         TreeNode parent;
 
-        public TreeNode(int key, TreeNode leftChild, TreeNode rightChild, TreeNode parent) {
+        TreeNode(int key, TreeNode leftChild, TreeNode rightChild, TreeNode parent) {
             this.key = key;
             this.leftChild = leftChild;
             this.rightChild = rightChild;
             this.parent = parent;
         }
 
-        public int getKey() {
+        int getKey() {
             return key;
         }
 
-        public boolean isEmpty() {
-            if (root == null) return true;
-            else return false;
+        boolean isEmpty() {
+
+            return root == null;
         }
     }
 
-    public void add(int key) {
+    private void add(int key) {
         TreeNode parentN = null;
         TreeNode newNode = new TreeNode(key, null, null, null);
         TreeNode pNode = root;
@@ -57,7 +54,7 @@ public class BinaryTree {
         }
     }
 
-    public TreeNode search(int key) {
+    TreeNode search(int key) {
         TreeNode p = root;
         while (p != null && p.key != key) {
             if (key < p.key) p = p.leftChild;
@@ -66,7 +63,7 @@ public class BinaryTree {
         return p;
     }
 
-    public TreeNode max(TreeNode node) {
+    private TreeNode max(TreeNode node) {
         TreeNode p = node;
         while (p.rightChild != null) {
             p = p.rightChild;
@@ -74,7 +71,7 @@ public class BinaryTree {
         return p;
     }
 
-    public TreeNode minElemNode(TreeNode node) {
+    TreeNode min(TreeNode node) {
         TreeNode pNode = node;
         while (pNode.leftChild != null) {
             pNode = pNode.leftChild;
@@ -82,13 +79,13 @@ public class BinaryTree {
         return pNode;
     }
 
-    public void remove(TreeNode p) {
+    void remove(TreeNode p) {
         if (p == null) return;
         if (p.leftChild == null && p.rightChild == null) {
             TreeNode parentNode = p.parent;
             if (p == parentNode.leftChild) parentNode.leftChild = null;
             else parentNode.rightChild = null;
-        } else if (p.leftChild == null && p.rightChild != null) {
+        } else if (p.leftChild == null) {
             TreeNode parentNode = p.parent;
             if (p == parentNode.leftChild) {
                 parentNode.leftChild = p.rightChild;
@@ -97,7 +94,7 @@ public class BinaryTree {
                 parentNode.rightChild = p.rightChild;
                 p.rightChild.parent = parentNode;
             }
-        } else if (p.leftChild != null && p.rightChild == null) {
+        } else if (p.rightChild == null) {
             TreeNode parentNode = p.parent;
             if (p == parentNode.leftChild) {
                 parentNode.leftChild = p.leftChild;
@@ -113,7 +110,7 @@ public class BinaryTree {
     }
 
 
-    public BinaryTree getTree(int[] keys) {
+    BinaryTree getTree(int[] keys) {
         BinaryTree a = new BinaryTree();
         for (int key : keys) {
             a.add(key);
@@ -121,22 +118,15 @@ public class BinaryTree {
         return a;
     }
 
-    private void inOrderTraverse(TreeNode root) {
-        if (root != null) {
-            inOrderTraverse(root.leftChild);
-            nodeList.add(root);
-            inOrderTraverse(root.rightChild);
-        }
-    }
 
-    public boolean isSameTree(TreeNode p, TreeNode q) {
+    boolean isSameTree(TreeNode p, TreeNode q) {
         if ((p == null && q != null) || (p != null && q == null)) {
             return false;
         }
-        if (p != null && q != null) {
+        if (p != null) {
             if (!(p.key == q.key)) return false;
         }
-        if (!(p == null && q == null)) {
+        if (!(p == null)) {
             if (!isSameTree(p.leftChild, q.leftChild)) {
                 return false;
             }
--- test/BinaryTreeTest.java
+++ test/BinaryTreeTest.java
@@ -1,6 +1,4 @@
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
